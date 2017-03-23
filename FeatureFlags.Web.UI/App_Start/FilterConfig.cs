c569cddee2a203c5744e841c354ad8c0e3ed6f0f
Kevin Mack <kevinmackdev@outlook.com>
2017-03-23 15:02:25 -0400

    Fixing app insights issue

--- FeatureFlags.Web.UI/App_Start/FilterConfig.cs
+++ FeatureFlags.Web.UI/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace FeatureFlags.Web.UI
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
